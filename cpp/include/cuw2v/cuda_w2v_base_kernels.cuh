ddd9095d92a25dd9ce42d428986eb46a21c2dd55
js1010 <vjs1010v@gmail.com>
2021-02-12 17:22:25 +0900

    separate files

---
+++ cpp/include/cuw2v/cuda_w2v_base_kernels.cuh
@@ -0,0 +1,55 @@
+// Copyright (c) 2021 Jisang Yoon
+// All rights reserved.
+//
+// This source code is licensed under the Apache 2.0 license found in the
+// LICENSE file in the root directory of this source tree.
+#pragma once
+#include "utils/cuda_utils_kernels.cuh"
+
+using thrust::random::default_random_engine;
+using thrust::random::uniform_int_distribution;
+
+namespace cusim {
+
+
+__inline__ __device__
+void PositiveFeedback(const float* vec1, float* vec2, float* grad, 
+    float& loss_nume, float& loss_deno, const int num_dims) {
+  static __shared__ float g;
+  float dot = Dot(emb_in[num_dims * j], emb_out[num_dims * k], num_dims);
+  if (threadIdx.x == 0) {
+    float exp_dot = expf(-dot);
+    g = exp_dot / (1 + exp_dot);
+    loss_nume += logf(1 + exp_dot);
+    loss_deno++;
+  }
+  __syncthreads();
+  for (int i = threadIdx.x; i < num_dims; i += blockDim.x) {
+    float tmp = vec2[i];
+    vec2[i] += vec1[i] * g;
+    grad[i] += tmp * g;
+  }
+  __syncthreads();
+}
+
+__inline__ __device__
+void NegativeFeedback(const float* vec1, float* vec2, float* grad, 
+    float& loss_nume, float& loss_deno, const int num_dims) {
+  static __shared__ float g;
+  float dot = Dot(emb_in[num_dims * j], emb_out[num_dims * k], num_dims);
+  if (threadIdx.x == 0) {
+    float exp_dot = expf(dot);
+    g = exp_dot / (1 + exp_dot);
+    loss_nume += logf(1 + exp_dot);
+    loss_deno++;
+  }
+  __syncthreads();
+  for (int i = threadIdx.x; i < num_dims; i += blockDim.x) {
+    float tmp = vec2[i];
+    vec2[i] -= vec1[i] * g;
+    grad[i] -= tmp * g;
+  }
+  __syncthreads();
+}
+
+}  // cusim
--- cpp/include/cuw2v/cuda_w2v_ns_kernels.cuh
+++ cpp/include/cuw2v/cuda_w2v_ns_kernels.cuh
@@ -5,52 +5,12 @@
 // LICENSE file in the root directory of this source tree.
 #pragma once
 #include "utils/cuda_utils_kernels.cuh"
+#include "w2v/cuda_w2v_base_kernels.cuh"
 
 using thrust::random::default_random_engine;
 using thrust::random::uniform_int_distribution;
 
 namespace cusim {
-
-
-__inline__ __device__
-void PositiveFeedback(const float* vec1, float* vec2, float* grad, 
-    float& loss_nume, float& loss_deno, const int num_dims) {
-  static __shared__ float g;
-  float dot = Dot(emb_in[num_dims * j], emb_out[num_dims * k], num_dims);
-  if (threadIdx.x == 0) {
-    float exp_dot = expf(-dot);
-    g = exp_dot / (1 + exp_dot);
-    loss_nume += logf(1 + exp_dot);
-    loss_deno++;
-  }
-  __syncthreads();
-  for (int i = threadIdx.x; i < num_dims; i += blockDim.x) {
-    float tmp = vec2[i];
-    vec2[i] += vec1[i] * g;
-    grad[i] += tmp * g;
-  }
-  __syncthreads();
-}
-
-__inline__ __device__
-void NegativeFeedback(const float* vec1, float* vec2, float* grad, 
-    float& loss_nume, float& loss_deno, const int num_dims) {
-  static __shared__ float g;
-  float dot = Dot(emb_in[num_dims * j], emb_out[num_dims * k], num_dims);
-  if (threadIdx.x == 0) {
-    float exp_dot = expf(dot);
-    g = exp_dot / (1 + exp_dot);
-    loss_nume += logf(1 + exp_dot);
-    loss_deno++;
-  }
-  __syncthreads();
-  for (int i = threadIdx.x; i < num_dims; i += blockDim.x) {
-    float tmp = vec2[i];
-    vec2[i] -= vec1[i] * g;
-    grad[i] -= tmp * g;
-  }
-  __syncthreads();
-}
 
 __global__ void W2VNegSgKernel(
   const int* cols, const int* indptr, const int window,
