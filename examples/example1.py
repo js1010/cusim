4b5be55896294961fd8b05e59c49d445d2330196
js1010 <vjs1010v@gmail.com>
2021-02-14 15:23:46 +0900

    update example

--- examples/example1.py
+++ examples/example1.py
@@ -96,12 +96,13 @@ def run_cusim_w2v():
     "num_dims": 100,
     "hyper_threads": 100,
     "epochs": 10,
-    "lr": 0.001,
+    "lr": 0.01,
     "io": {
       "lower": False
     },
-    "neg": 10,
-    "skip_gram": True,
+    "neg": 0,
+    "skip_gram": False,
+    "cbow_mean": False,
     # "pretrained_model": {
     #   "filename": "./res/gensim.w2v.model",
     # }
@@ -117,8 +118,9 @@ def run_cusim_w2v():
 def run_gensim_w2v():
   start = time.time()
   model = gensim.models.Word2Vec(corpus_file=DATA_PATH, min_alpha=0.001,
-                                 min_count=5, sg=True, hs=False, workers=8,
-                                 alpha=0.001, negative=10, iter=10)
+                                 min_count=5, sg=False, hs=True, workers=8,
+                                 alpha=0.001, negative=10, iter=10,
+                                 cbow_mean=False)
   LOGGER.info("elapsed for gensim w2v training: %.4e sec", time.time() - start)
   model.wv.save_word2vec_format(GENSIM_W2V_PATH, binary=False)
   LOGGER.info("gensim w2v model is saved to %s", GENSIM_W2V_PATH)
