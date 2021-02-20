83c743cc350d7d526b30932f8c6209575a6e45a0
js1010 <vjs1010v@gmail.com>
2021-02-20 15:20:34 +0900

    pip install cusim

--- doc/conf.py
+++ doc/conf.py
@@ -19,14 +19,14 @@
 
 # -- Project information -----------------------------------------------------
 import sphinx_rtd_theme
-
+from cusim import version
 
 project = 'cusim'
 copyright = '2021, Jisang Yoon'
 author = 'Jisang Yoon'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1v'
+release = version.short_version
 
 
 # -- General configuration ---------------------------------------------------
