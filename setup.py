92e47cbcbc49e7a5f5e14ed642168ce2bffd42cf
js1010 <vjs1010v@gmail.com>
2021-02-20 20:12:45 +0900

    add performance table

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ with open("requirements.txt", "r") as fin:
 
 MAJOR = 0
 MINOR = 0
-MICRO = 1
+MICRO = 2
 RELEASE = True
 STAGE = {True: '', False: 'b'}.get(RELEASE)
 VERSION = f'{MAJOR}.{MINOR}.{MICRO}{STAGE}'
