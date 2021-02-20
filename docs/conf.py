fbb487de3bc69b7788b7158527b4d7e638d0c8fa
js1010 <vjs1010v@gmail.com>
2021-02-20 18:18:24 +0900

    add autodoc ext

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,9 @@ release = "0.0.1"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-"sphinx_rtd_theme"
+  "sphinx_rtd_theme",
+  "sphinx.ext.autodoc",
+  "sphinx.ext.napoleon"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
